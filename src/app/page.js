0694775f02a99080bd363e543e8113aa01204b62
Midhun Nair <midhunnair01@yahoo.com>
2024-03-31 10:00:24 +0530

    fix(Homepage): Padding issue in mobile devices
    Fixed Padding issue in mobile devices

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import { HeroSection, ServiceSection } from "@components/home";
 
 export default async function Home() {
   return (
-    <div className="container mx-auto p-12 flex flex-col gap-y-16">
+    <div className="container mx-auto p-12 max-sm:p-6 flex flex-col gap-y-16">
       <HeroSection />
       <ServiceSection />
     </div>
